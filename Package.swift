19d6f34aede8bb310713c287a82165d0d08a868c
runner <runner@Mac-1666301451499.local>
2022-10-20 21:37:33 +0000

    KMM SPM package release for 1.0.3

--- Package.swift
+++ Package.swift
@@ -1,6 +1,8 @@
 // swift-tools-version:5.3
 import PackageDescription
 
+let remoteKotlinUrl = "https://api.github.com/repos/Shahroz16/pgkmm/releases/assets/81723229.zip"
+let remoteKotlinChecksum = "4c5cfb46f82344e1d363c24f4717051f973db80ec75dcf088e9ce065ee813f2d"
 let packageName = "shared"
 
 let package = Package(
@@ -17,7 +19,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: packageName,
-            path: "./shared/build/XCFrameworks/debug/\(packageName).xcframework"
+            url: remoteKotlinUrl,
+            checksum: remoteKotlinChecksum
         )
         ,
     ]
