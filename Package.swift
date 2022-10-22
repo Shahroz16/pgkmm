64b23fe5031795cf30c6c4ed9bccd28b19783bf1
runner <runner@Mac-1666452420760.local>
2022-10-22 16:09:17 +0000

    KMM SPM package release for 1.0.8

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let remoteKotlinUrl = "https://api.github.com/repos/Shahroz16/pgkmm/releases/assets/81915482.zip"
+let remoteKotlinChecksum = "a6a264a2aa099973e23ee82d0f4ca234955c4d8b6475cc19b47c9133e8ff6cba"
+let packageName = "shared"
+
+let package = Package(
+    name: packageName,
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: packageName,
+            targets: [packageName]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: packageName,
+            url: remoteKotlinUrl,
+            checksum: remoteKotlinChecksum
+        )
+        ,
+    ]
+)
